910e28e384b78279a7b81530a748230ce00c304b
felix1805 <felix1805@gmail.com>
2023-10-27 16:09:56 -0400

    initialized react app, removed unneeded files and content, added intial API logic

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react-air-quality-application
+this app uses react along with an air quality api in order to obtain city-based air quality information.
---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './App.css';
+
+
+function App() {
+  const [airQualityData, setAirQualityData] = useState(null)
+  const [error, setError] = useState(null)
+
+  const getAirQuality = async (city) => {
+    try {
+      const response = await fetch(`https://api.waqi.info/feed/${city}/?token=${process.env.REACT_APP_AQI_TOKEN}`)
+      const data = await response.json()
+      console.log(data)
+      if (response.ok && data.status === 'ok') {
+        setAirQualityData(data.data)
+        setError(null)
+      } else {
+        setError("The location you have entered could not be found. Please verify correct spelling or enter another city.")
+        setAirQualityData(null)
+      }
+    } catch (error) {
+      console.error("API error:", error)
+      setError('Sorry, something went wrong')
+      setAirQualityData(null)
+    }
+  }
+  return (
+    <div>
+      <h1>Air Quality Index Checker</h1>
+      <CitySearch getAirQuality={getAirQuality} />
+    </div>
+  );
+}
+
+export default App;
